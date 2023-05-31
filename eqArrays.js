35d3ddd115a21c7b9a15d18e5e7e32bc320ecd39
adavidliang <adavidliang@gmail.com>
2023-05-31 15:53:07 -0400

    make it more runable

--- eqArrays.js
+++ eqArrays.js
@@ -7,12 +7,15 @@ const assertEqual = function (actual, expected) {
 };
 
 const eqArrays = function (arr1, arr2) {
+  if(arr1.length != arr2.length){
+    return false
+  }
   for (let i in arr1) {
     if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
-  return true
+  return true;
 }
 
 //test
