d819bae9baf3e87dea073aac539e612a102585cf
adavidliang <adavidliang@gmail.com>
2023-05-31 15:15:15 -0400

    equal each other

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌❌❌  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (arr1, arr2) {
+  for (let i in arr1) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true
+}
+
+//test
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
+
