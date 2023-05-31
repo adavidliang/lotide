4049287e095e40ae4bcf1e7be10aaa27d89777df
adavidliang <adavidliang@gmail.com>
2023-05-31 15:53:49 -0400

    better compare

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const eqArrays = function (arr1, arr2) {
+  if(arr1.length != arr2.length){
+    return false
+  }
+  for (let i in arr1) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌  Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+assertArraysEqual([1,2], [1,2]);
+  assertArraysEqual([2,2], [1,2]);
+  assertArraysEqual([], [1, 2]);
